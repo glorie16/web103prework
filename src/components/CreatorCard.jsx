79605ca2811668c393e7d583cb42c24725fff83a
glorie16 <gcmendoza@cpp.edu>
2026-01-29 21:51:44 -0800

    added more css styling

--- src/components/CreatorCard.jsx
+++ src/components/CreatorCard.jsx
@@ -1,10 +1,34 @@
 import './CreatorCard.css'
+import instaIcon from '../assets/icons/instagram.png'
+import ytIcon from '../assets/icons/youtube.png'
 
 function CreatorCard(props){
     return(
-        <div className="creator-card">
-            <h2 className="name">{props.name}</h2>
-            <p className="description">{props.description}</p>
+        <div className="creator-card"
+            style={{backgroundImage: `url(${props.imageURL})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    }}>
+            <div className="card-content">
+                <div className="top-row">
+                    <h2 className="name">{props.name}</h2>
+                    <div className="btn-container">
+                        <button className="yt-btn" alt="like">
+                            <img src={ytIcon}/>
+                        </button>
+                        <button className="insta-btn">
+                            <img src={instaIcon}>
+                            </img>
+                        </button>
+               
+                </div>
+            </div>
+           
+           <div className="bottom-row">
+                <p className="description">{props.description}</p>
+            </div>
+                 </div>
         </div>
     )
 }
